18027bdacfedebc46b49538d590ecae819b4ea3d
Kiyarash <kiarashnadri@yahoo.com>
2023-03-03 18:51:03 +0330

    use Router with Navbar component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import './App.css';
-import Navbar from './Navbar';
+// import Navbar from './Navbar';
 import Home from './Home';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 import Create from './Create';
 import Name from './Name';
 
@@ -11,7 +11,22 @@ function App() {
     <Router>
       <div className="App">
 
-        <Navbar></Navbar>
+        {/* <Navbar></Navbar> */}
+
+        <nav className="navbar">
+          <h1>The dojo blog</h1>
+          <div className="Links">
+            <Link to="/">Home</Link>
+
+            <Link to="/create" style={{
+              color: 'white',
+              backgroundColor: '#f1356d',
+              borderRadius: '8px'
+            }}>New blog</Link>
+
+            <Link to="/name">Name</Link>
+          </div>
+        </nav>
 
         <div className="content">
           <Switch>
